11903ce6f517eb7f6c7b7043c999fdeeec432bb5
vinaykhanagavi <vinaykhanagavi502@gmail.com>
2024-08-18 21:16:51 +0530

    Changes to be committed: 	modified:   components/component/landing-page.jsx 	modified:   middleware.ts

--- components/component/landing-page.jsx
+++ components/component/landing-page.jsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button"
 import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/sheet"
 import { Card, CardHeader, CardContent, CardFooter } from "@/components/ui/card"
 import { useRouter } from 'next/navigation';
- 
+
 
 import {
     ClerkProvider,
